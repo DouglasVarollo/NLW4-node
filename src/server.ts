67c70d8f89ec5f5a45104949f2ca18baabb2d17b
Douglas Varollo <douglasvarollo@gmail.com>
2021-02-22 18:45:44 -0300

    Add sample routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,18 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', function (request, response) {
+  return response.json({
+    message: 'Hello World - NLW04'
+  });
+});
+
+app.post('/', function (request, response) {
+  return response.json({
+    message: 'Os dados foram salvos com sucesso!'
+  });
+});
+
 app.listen(3333, function () {
   console.log('Server is running!');
 });

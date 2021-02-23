9615bbec5718c8c0f1f3e571b1a64a650da9fa8c
Douglas Varollo <douglasvarollo@gmail.com>
2021-02-23 17:14:49 -0300

    Add users creation

--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,12 @@ import 'reflect-metadata';
 import express from 'express';
 
 import './database';
+import router from './routes';
 
 const app = express();
 
-app.get('/', function (request, response) {
-  return response.json({
-    message: 'Hello World - NLW04'
-  });
-});
-
-app.post('/', function (request, response) {
-  return response.json({
-    message: 'Os dados foram salvos com sucesso!'
-  });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, function () {
   console.log('Server is running!');

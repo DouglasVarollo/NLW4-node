acacd654ba86b9f72bd549e4bd3a3f7fca8cd7b6
Douglas Varollo <douglasvarollo@gmail.com>
2021-02-26 17:50:24 -0300

    Send the survey by email

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UsersController } from './controllers/UsersController';
 
+const sendMailController = new SendMailController();
 const surveysController = new SurveysController();
 const usersController = new UsersController();
 const router = Router();
+
+router.post('/sendMail', sendMailController.execute);
 
 router.get('/surveys', surveysController.index);
 router.post('/surveys', surveysController.create);

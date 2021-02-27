f7827fe4f32d383746a4e83241c37f63a0d78688
Douglas Varollo <douglasvarollo@gmail.com>
2021-02-27 16:30:44 -0300

    Add Answer controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UsersController } from './controllers/UsersController';
 
+const answerController = new AnswerController();
 const sendMailController = new SendMailController();
 const surveysController = new SurveysController();
 const usersController = new UsersController();
 const router = Router();
+
+router.get('/answers/:value', answerController.execute);
 
 router.post('/sendMail', sendMailController.execute);
 
